657a79a0630e3f1527bd3f74fe1061cd34c87abb
SdangondLT <sdangond@lean-tech.io>
2022-04-24 21:37:27 -0500

    angular project created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    loadChildren: () => import('./layouts/main-layout/main-layout.module')
+    .then(m => m.MainLayoutModule)
+  },
+  {
+    path: "dashboard",
+    loadChildren: () => import('./modules/dashboard/dashboard.module')
+    .then(m => m.DashboardModule)
+  },
+  {
+    path: "profile",
+    loadChildren: () => import('./modules/profile/profile.module')
+    .then(m => m.ProfileModule)
+  },
+  {
+    path: "schedule",
+    loadChildren: () => import('./modules/schedule/schedule.module')
+    .then(m => m.ScheduleModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
